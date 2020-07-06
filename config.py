14adef7b7e029c9de41f8e832fe29ac3d2961c34
sidney-wells <67520606+sidney-wells@users.noreply.github.com>
2020-07-06 13:13:40 -0500

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Raymyth6!@localhost:5432/fyyurapp'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:@localhost:5432/fyyurapp'
